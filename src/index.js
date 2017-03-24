83c3607b623df3ea04b87e15f6cbd277c22f9f73
jeanmw <jean.weatherwax@gmail.com>
2017-03-23 21:17:13 -0700

    more scaffold fixes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 
-ReactDOM.render(
-  <CommentBox
-    url='http://localhost:3001/api/comments'
-    pollInterval={2000} />,
-  document.getElementById('root')
-);
+//more React code here!
