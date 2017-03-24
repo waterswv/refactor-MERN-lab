7e4527598c6d2ed1f16a999c0f8d8b1f383ed207
jeanmw <jean.weatherwax@gmail.com>
2017-03-23 17:17:54 -0700

    getting starter code ready

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const data = [
-  { id: 1, author: 'Bryan', text: 'Wow this is neat!'},
-  { id: 2, author: 'You', text: 'youre __right!__'}
+  { id: 1, author: 'Jean', text: 'Wow this is neat!'},
+  { id: 2, author: 'Justin', text: 'youre __right!__'}
 ]
 
 module.exports = data;
