fd6e224e53dd124f230a3ebc98bf7c34cbfefdb5
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-08-07 16:01:57 -0700

    completed setup of displaying db comments & new comment form display

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,28 +1,41 @@
-import React, {Component} from 'react';
+//CommentBox.js
+import React, { Component } from 'react';
+import $ from 'jquery-ajax';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
-
 import style from './style';
 
 class CommentBox extends Component {
-
   constructor(props) {
     super(props)
-
-    this.state = {
-      data: []
-    }
+    this.state = { data: []  };
+    this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
   }
-
+  loadCommentsFromServer() {
+    $.ajax({
+      method: 'GET',
+      url: this.props.url
+    })
+    .then((res) => {
+      this.setState({ data: res });
+    }, (err) => {
+      console.log('error', err)
+    })
+  }
+  handleCommentSubmit(comment) {
+    //add POST request
+  }
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  }
   render() {
-
     return (
-
       <div style={ style.commentBox }>
-
-        <CommentList
-          data={ this.state.data } />
-        <CommentForm />
+          <h2>Comments:</h2>
+        <CommentList data={ this.state.data }/>
+        <CommentForm onCommentSubmit={ this.handleCommentSubmit }/>
 
       </div>
     )
