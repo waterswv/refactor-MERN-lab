8a1e55d1b352ead7da399ff73640e1644aa0a6b9
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-08-07 14:36:01 -0700

    created CommentBox component container. This will house our comments for the App

---
+++ src/CommentBox.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import CommentList from './CommentList';
+import CommentForm from './CommentForm'
+import DATA from '../data';
+import style from './style';
+
+class CommentBox extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: []
+    }
+  }
+
+  render() {
+
+    return (
+
+      <div style={ style.commentBox }>
+
+        <CommentList
+          data={ DATA } />
+        <CommentFrom />
+
+      </div>
+    )
+  }
+}
+
+export default CommentBox
